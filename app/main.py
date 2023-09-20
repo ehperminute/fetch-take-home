269f707d82bc958fe8b5b0ae0bb94739b906b361
ehperminute <87844955+ehperminute@users.noreply.github.com>
2023-09-19 22:32:24 -0600

    Update main.py
    minor linting

--- app/main.py
+++ app/main.py
@@ -4,11 +4,13 @@ from config import FIELDS_TO_HASH
 from postgres import entry_to_tuple, insert_postgres, change_column_type
 from time import sleep
 
+
 #replaces cells values in the masked columns by their hashes
 def mask_data(data_entry, fields=FIELDS_TO_HASH):
     for field in FIELDS_TO_HASH:
         if field in data_entry and data_entry[field] != 'NULL':
             data_entry[field] = md5(data_entry[field].encode()).hexdigest()
+
 
 def mainloop():
     if poll():
@@ -17,6 +19,7 @@ def mainloop():
             mask_data(entry)
         insert_postgres(entries)
     sleep(10)
+
 
 def main():
     change_column_type()
