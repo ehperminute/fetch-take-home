795b8e2fecb1bde4b7bdefcd744625f5af267d57
ehperminute <87844955+ehperminute@users.noreply.github.com>
2023-09-19 22:32:47 -0600

    Update postgres.py
    linting

--- app/postgres.py
+++ app/postgres.py
@@ -14,6 +14,7 @@ INSERT INTO user_logins(
     create_date)
 VALUES %s'''
 
+
 def entry_to_tuple(entry):
     return tuple(entry.get(field, 'NULL') for field in (
         'user_id',
@@ -25,18 +26,21 @@ def entry_to_tuple(entry):
         )
     ) + ('NOW()',)
 
+
 def db_connect(dbname=DBNAME, user=DBUSER, password=PASSWORD):
     conn = psycopg2.connect(dbname=dbname, user=user, password=password)
     cursor = conn.cursor()
     return conn, cursor
-    
+
+
 def change_column_type(column='app_version', new_type='varchar(20)'):
     conn, cursor = db_connect()
     cursor = conn.cursor()
     cursor.execute(f'ALTER TABLE user_logins ALTER COLUMN {column} TYPE {new_type}')
     conn.commit()
     conn.close()
-    
+
+
 def insert_postgres(entries):
     conn, cursor = db_connect()
     data_to_insert = tuple(entry_to_tuple(entry) for entry in entries)
